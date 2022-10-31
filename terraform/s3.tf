46b5995d5d00f660bcca3a5365aa97059bda7aee
Supraj Maripeddi <suprajmaripeddi@C02FN3Z4Q6LR.local>
2022-10-31 16:43:35 +0530

    adding network bucket

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -1,3 +1,6 @@
+data "aws_caller_identity" "current" {}
+
+
 resource "aws_s3_bucket" "core-ou-shared" {
   bucket = "aft-core-ou-${data.aws_caller_identity.current.account_id}-network-account"
   acl    = "private"
